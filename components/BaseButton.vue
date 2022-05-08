294e1ac39b8857e7c60c1bc9cb7f78da11fa2edb
daver987 <daveyalexander@outlook.com>
2022-05-08 00:40:18 -0400

    Add: Button component

---
+++ components/BaseButton.vue
@@ -0,0 +1,17 @@
+<template>
+  <button
+    type="button"
+    class="inline-flex justify-center items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md shadow-sm text-white bg-primary/90 hover:bg-primary focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-primary/80 uppercase tracking-widest"
+  >
+    {{ label }}
+  </button>
+</template>
+
+<script setup lang="ts">
+defineProps({
+  label: {
+    type: String,
+    default: 'Button',
+  },
+})
+</script>
